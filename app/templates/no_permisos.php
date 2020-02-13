f300caa5d7ac954d504b13287fd8bec84bd06c9b
jaimein <jaimeiserte@hotmail.com>
2020-02-13 10:44:16 +0100

    añadimos vista no_permisos

---
+++ app/templates/no_permisos.php
@@ -0,0 +1,18 @@
+<?php ob_start() ?>
+
+<div class="row">
+
+<h3> No tiene permisos para acceder </h3>
+
+
+<?php $contenido = ob_get_clean() ?>
+
+<!-- Cambiar si al final pasamos el usuario en $params-->
+<!--  if (isset($params['mensaje'])):  -->
+<?php
+ if (isset($_SESSION['id'])) {
+    include 'layout.php';
+ } else {
+    include 'layout_basico.php'; 
+ }
+ ?>
